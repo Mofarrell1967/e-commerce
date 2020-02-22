965b8164e6f7bcf70e1ca05fc90eeb53e4645d0d
Michael O'Farrell <mofarrell1967@gmail.com>
2020-02-22 12:59:08 +0000

    Updated settings.py with heroku allowed hosts

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = [os.environ.get('C9_HOSTNAME'),
-                ('localhost')]
+                ('localhost'), 'mo-ecommerce-app.herokuapp.com']
 
 
 # Application definition
